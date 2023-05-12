00d8e5aad890516a5d03522c291a4e26fa3ed333
Priiyyya-21 <priyamajhi79@gmail.com>
2023-05-13 01:27:39 +0530

    Added Scroll bar

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// scroll bar
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
